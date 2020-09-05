d2d958fddc75710764292c166d85e1139c3e5d4d
Kush Daga <kushdaga@hotmail.com>
2020-09-05 14:08:08 +0530

    finished singly linked liost

--- Linked Lists/singlyLinkedList.js
+++ Linked Lists/singlyLinkedList.js
@@ -38,8 +38,10 @@ class LinkedList {
       array.push(currentNode.value);
       currentNode = currentNode.next;
     }
+    console.log("Linked List", array);
     return array;
   }
+  // Kush's Implementation
   insert(index, value) {
     //If at beginning just prepend
     if (index === 0) {
@@ -67,16 +69,66 @@ class LinkedList {
     }
     return this.printList();
   }
+
+  //Course Implementation
+  insertCourse(index, value) {
+    //If index 0
+
+    if (index === 0) {
+      this.prepend(value);
+      return this.printList();
+    }
+
+    //check params
+
+    if (index >= this.length) {
+      // Add at the end
+      return this.append(value);
+    }
+
+    const newNode = new Node(value);
+    const leader = this.traverseToIndex(index - 1); //prevNode in kush implementation
+    const holdingPointer = leader.next; //currentNode in kush implementation
+
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+
+    this.length++;
+
+    return this.printList();
+  }
+
+  //Deleting method
+
+  remove(index) {
+    //check params //TODO
+    const leader = this.traverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
+  }
+
+  traverseToIndex(index) {
+    //check params
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    return currentNode;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.prepend(15);
-console.log(myLinkedList.printList());
+// console.log(myLinkedList.printList());
 myLinkedList.insert(0, 10);
-console.log(myLinkedList.printList());
+// console.log(myLinkedList.printList());
 myLinkedList.insert(2, 0);
 myLinkedList.insert(4, 20);
 myLinkedList.insert(10, 110); // Should not do anything, as index not present in linked list
 
-console.log(myLinkedList.printList());
+// console.log(myLinkedList.printList());
